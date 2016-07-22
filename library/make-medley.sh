a8c3547bc8da0d19bd4eb58cf800e94b29e26006
Nick Partridge <nkpart@gmail.com>
2016-07-23 08:16:52 +1000

    bbcoca scores

--- library/make-medley.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-score-writer render --landscape --score-file lady-mackenzie-of-fairbairn.score --output-file lady-mackenzie.pdf
-
-/usr/local/bin/gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=medley.pdf lady-mackenzie.pdf
