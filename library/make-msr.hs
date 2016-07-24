127dea59c72a4adaba4649953b421f05fc696b16
Nick Partridge <nkpart@gmail.com>
2016-07-25 08:51:28 +1000

    updates to oca scores format

--- library/make-msr.hs
+++ library/make-msr.hs
@@ -5,10 +5,21 @@
 
 import Turtle
 import Filesystem.Path.CurrentOS
+import Data.Text
 
 main = sh $
-    do m <- using $ mktempfile "." "score.pdf"
-       sr <- using $ mktempfile "." "score.pdf"
-       shells ("score-writer render --portrait --score-file bbc-oca/msr/79ths.score --output-file " <> encode m) mempty
-       shells ("score-writer render --portrait --score-file bbc-oca/msr/dorrator.score --score-file bbc-oca/msr/lexy.score --output-file " <> encode sr) mempty
+    do m <- using $ mktempfile "." "m.pdf"
+       sr <- using $ mktempfile "." "sr.pdf"
+       let p1 = ["bbc-oca/msr/79ths.score"]
+           p2 = ["bbc-oca/msr/dorrator.score", "bbc-oca/msr/lexy.score"]
+       p1f <- encode <$> using (mktempfile "." "p1.score")
+       p2f <- encode <$> using (mktempfile "." "p2.score")
+       shells ("cat " <> (intercalate " " p1) <> " > " <> p1f) mempty
+       shells ("cat " <> "styles/landscape.score " <> (intercalate " " p2) <> " > " <> p2f) mempty
+
+       procs "score-writer" ["render", "--score-file", p1f, "--output-file", encode m] mempty
+       procs "score-writer" ["render", "--score-file", p2f, "--output-file", encode sr] mempty
+
+       -- shells ("score-writer render --portrait --score-file bbc-oca/msr/inner-guard.score --output-file " <> encode m) mempty
+       -- shells ("score-writer render --portrait --score-file bbc-oca/msr/dorrator.score --score-file bbc-oca/msr/lexy.score --output-file " <> encode sr) mempty
        shells ("/usr/local/bin/gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=msr.pdf " <> encode m <> " " <> encode sr) mempty
